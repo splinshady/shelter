9084a2bc8e58277a444ee9f24599661696cdf782
splinshady <splin.shady@gmail.com>
2022-12-17 23:22:53 +0300

    refactor: slider

--- pages/pets/pets.js
+++ pages/pets/pets.js
@@ -2,6 +2,9 @@ import {data} from "../main/data.js";
 import {Modal} from "../main/modal.js";
 
 const wrapper = document.querySelector('.pets-our-friends__slider');
+const burger = document.querySelector('.burger');
+const headerNavigation = document.querySelector('.header__navigation');
+const navigation = document.querySelector('.navigation');
 
 
 wrapper.addEventListener('click', (event) => {
@@ -11,3 +14,12 @@ wrapper.addEventListener('click', (event) => {
   let modal = new Modal(dataCard);
   modal.buildModal();
 })
+
+if (burger) {
+  burger.addEventListener('click', (event) => {
+    document.body.classList.toggle('_lock');
+    burger.classList.toggle('burger_active');
+    headerNavigation.classList.toggle('header__navigation_active');
+    navigation.classList.toggle('navigation_active');
+  })
+}
